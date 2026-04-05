1a27f8ea689d9aab2c832cf4588fc44c2e952c25
Gevin Krishnan S <gevinks@gmail.com>
2026-04-05 11:36:02 +0530

    Base FileStorage decorator

--- src/main/java/com/gevin/kvstore/FileStorageDecorator.java
+++ src/main/java/com/gevin/kvstore/FileStorageDecorator.java
@@ -1,19 +1,50 @@
 package com.gevin.kvstore;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Optional;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileStorageDecorator implements StorageEngine{
 
-    private InMemoryStorage inMemoryStorage;
+    private StorageEngine inMemoryStorage;
 
     private FileWriter writeAheadLog;
     
-    public FileStorageDecorator() {
+    public FileStorageDecorator(StorageEngine inMemoryStorage) {
+        this.inMemoryStorage = inMemoryStorage;
         try {
-            writeAheadLog = new FileWriter("wal.log");
-            inMemoryStorage = new InMemoryStorage();
+            Path walPath = Paths.get("storage", "wal.log");
+            Path storageDirectory = Paths.get("storage");
+            if (!Files.exists(storageDirectory)) {
+                try {
+                    Files.createDirectories(storageDirectory);
+                }
+                catch (IOException e) {
+                    System.err.println("Couldn't create directory : " + e.getMessage());
+                }
+            }
+            if (Files.exists(walPath) && !Files.isDirectory(walPath)) {
+                //read from disk and write to RAM
+                try {
+                    BufferedReader reader = new BufferedReader(new FileReader("storage/wal.log"));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        callAssignedMethod(line);
+                    }
+                    reader.close();
+                }
+                catch (IOException e) {
+                    System.err.println("Failed reading file : " + e.getMessage());
+                }
+            }
+            else if (Files.isDirectory(walPath)) {
+                throw new IOException("File is a directory");
+            }
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -52,6 +83,51 @@ public class FileStorageDecorator implements StorageEngine{
         } 
         catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void callAssignedMethod(String line) {
+        if (line.isEmpty())
+                return;
+        String key = "", value = "";
+        if (line.charAt(0) == 'p') {
+            int ind = 7;
+            while ((ind < line.length()) && !((ind < (line.length() - 1)) && (line.charAt(ind) != '|') && (line.charAt(ind + 1) != '|'))){
+                key = key + line.charAt(ind);
+                ind++;
+            }
+            ind++;
+            while ((ind < line.length())){
+                value = value + line.charAt(ind);
+                ind++;
+            }
+            put(key, value);
+        }
+        else if (line.charAt(0) == 'g') {
+            int ind = 7;
+            while ((ind < line.length()) && !((ind < (line.length() - 1)) && (line.charAt(ind) != '|') && (line.charAt(ind + 1) != '|'))){
+                key = key + line.charAt(ind);
+                ind++;
+            }
+            ind++;
+            while ((ind < line.length())){
+                value = value + line.charAt(ind);
+                ind++;
+            }
+            get(key);
+        }
+        else if (line.charAt(0) == 'd') {
+            int ind = 10;
+            while ((ind < line.length()) && !((ind < (line.length() - 1)) && (line.charAt(ind) != '|') && (line.charAt(ind + 1) != '|'))){
+                key = key + line.charAt(ind);
+                ind++;
+            }
+            ind++;
+            while ((ind < line.length())){
+                value = value + line.charAt(ind);
+                ind++;
+            }
+            delete(key);
         }
     }
 }

4aa11252a83d67a284a24b43329a638080041e5d
Gevin Krishnan S <gevinks@gmail.com>
2026-04-07 17:43:45 +0530

    Added base proto file

--- src/main/java/com/gevin/kvstore/App.java
+++ src/main/java/com/gevin/kvstore/App.java
@@ -1,4 +1,9 @@
 package com.gevin.kvstore;
 
 public class App {
+    public static void main(String[] args) {
+        StorageEngine engine = new InMemoryStorage();
+        FileStorageDecorator mainCache = new FileStorageDecorator(engine);
+
+    }
 }
